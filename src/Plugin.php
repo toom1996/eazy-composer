59186cbcb6a54aacbead0f68130ca1396890bc33
1023150697@qq.com <1023150697@qq.com>
2021-09-23 23:29:07 +0800

    first commit

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,7 +10,7 @@ class Plugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
-
+        die('3333');
     }
 
     public function deactivate(Composer $composer, IOInterface $io)
