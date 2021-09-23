9387d871fae01c526d908dc97c51bbcc584ae0d5
1023150697@qq.com <1023150697@qq.com>
2021-09-23 23:28:24 +0800

    first commit

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,12 +2,24 @@
 
 namespace eazy\composer;
 
+use Composer\Composer;
+use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class Plugin extends PluginInterface
+class Plugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
 
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement deactivate() method.
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement uninstall() method.
     }
 }
